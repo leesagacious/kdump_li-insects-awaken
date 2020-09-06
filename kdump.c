c803580368b65c805956b8dcbff135476326ad5f
leesagacious <lizhe@cpu-os.ac.cn>
2020-09-06 03:55:17 -0700

    The second kernel start point.
    Signed-off-by: leesagacious <lizhe@cpu-os.ac.cn>

--- kdump.c
+++ kdump.c
@@ -3,4 +3,17 @@
 	The default value is -1, otherwise, 
 	it means that the second capture kernel was started because kdump occurred.
 */
-unsigned long long elfhead_address = NOT_KDUMP_SETUP; 
+unsigned long long elfhead_address = NOT_KDUMP_SETUP;
+
+static int __init capturekernel_startpoint(void)
+{
+	int err_val = 0;
+
+	if (elfhead_address != NOT_KDUMP_SETUP) {
+		pr_err("the 2 kernel is booting after not panic\n");
+		pr_err("Bye, haha!!\n");
+		BUG();
+	}
+}
+
+core_initcall(capturekernel_startpoint); 
