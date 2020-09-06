e05213630388f02305f82cb2d07421af1f5630e6
leesagacious <lizhe@cpu-os.ac.cn>
2020-09-06 03:38:43 -0700

    In the second kernel, determine whether it is the second kernel started because of kdump
    Signed-off-by: leesagacious <lizhe@cpu-os.ac.cn>

---
+++ kdump.c
@@ -0,0 +1,6 @@
+
+/*
+	The default value is -1, otherwise, 
+	it means that the second capture kernel was started because kdump occurred.
+*/
+unsigned long long elfhead_address = NOT_KDUMP_SETUP; 
---
+++ kdump.h
@@ -0,0 +1,3 @@
+#ifdef COFNIG_KDUMP_START
+	#define NOT_KDUMP_SETUP	(-lUL)
+#endif
