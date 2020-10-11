1b3d43b0d189e58c81d38a16cb1133aa797279bf
leesagacious <lizhe@cpu-os.ac.cn>
2020-10-11 07:08:08 -0700

    Adjust the style of source code
    Signed-off-by: leesagacious <lizhe@cpu-os.ac.cn>

--- kdump.c
+++ kdump.c
@@ -1,20 +1,24 @@
+// SPDX-License-Identifier: BSD-2-Clause
+/*
+ * Copyright (c) 2020, li-insects-awaken
+ */
 #include <linux/errno.h>
 
 /*
-	Each item on the linked list is a memory range that crashes the kernel memory
-*/
-static struct list_head targets = LIST_HEAD_INIT(crash_memory_range);
+ *	Each item on the linked list is a memory range that crashes the kernel memory
+ */
+static struct list_head crash_memory_range = LIST_HEAD_INIT(crash_memory_range);
 
 /*
-	The default value is -1, otherwise, 
-	it means that the second capture kernel was started because kdump occurred.
-*/
+ *	The default value is -1, otherwise
+ *	it means that the second capture kernel was started because kdump occurred
+ */
 unsigned long long elfhead_address = NOT_KDUMP_SETUP;
 
 static ssize_t type_show(struct kobject *kobj,
-                         struct kobj_attribute *attr, char *buf)
+			struct kobj_attribute *attr, char *buf)
 {
-        return 0;
+	return 0;
 }
 
 static struct kobj_attribute type_attr = __ATTR_RO(type);
@@ -24,7 +28,7 @@ static int __init capturekernel_startpoint(void)
 	int ret;
 
 	if (elfhead_address != NOT_KDUMP_SETUP) {
-		pr_err("the 2 kernel is booting after not panic ! Bye Bye haha. \n");
+		pr_err("the 2 kernel is booting after not panic!");
 		return -EINVAL;
 		BUG();
 	}
@@ -35,4 +39,4 @@ static int __init capturekernel_startpoint(void)
 	return ret;
 }
 
-core_initcall(capturekernel_startpoint); 
+core_initcall(capturekernel_startpoint);
